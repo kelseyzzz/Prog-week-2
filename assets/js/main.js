09ea4ccb0f9e423a60f73a7d193ce6eaeb89cdc1
Kelsey Z <kelsey@Kelseys-MacBook-Pro.local>
2017-02-01 02:42:05 -0500

    hi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 
 
 function createToDoItem(toDoList, toDoValue) {
-	var toDoItem = document.createElement("li"); 
+	var toDoItem = document.createElement("li");
+	var textContainer = document.createElement("span"); 
 
-	toDoItem.innerHTML = toDoValue;
+	
 	toDoItem.classList.add("todo-item");
 
 	toDoList.appendChild(toDoItem);
@@ -14,18 +15,97 @@ function createToDoItem(toDoList, toDoValue) {
             checkbox.name = "todo[]";
         
         toDoItem.appendChild(checkbox);
+        textContainer.innerHTML = toDoValue;
+        toDoItem.appendChild(textContainer);
+
+    var deleteButton = document.createElement("button");
+    	toDoItem.appendChild(deleteButton);
+    	deleteButton.innerHTML = " X ";
+    	
 
 	checkbox.addEventListener("click", done);
+	deleteButton.addEventListener("click", deleteItem);
+	updateCounter();
+
+}
+
+// Give the list a Done class
+function done() {
+	var listItem = this.parentElement;
+	listItem.classList.toggle("done");
+
+	updateCounter();
+}
+
+function createThreeButtons() {
+	var markAll = document.createElement("button");
+	var mAll = document.querySelector(".mark-all");
+    	mAll.appendChild(markAll);
+    	markAll.innerHTML = " Mark all ";
+
+    var showAll = document.createElement("button");
+	var sAll = document.querySelector(".mark-all");
+    	sAll.appendChild(showAll);
+    	showAll.innerHTML = " Show all ";
+
+    var deleteAll = document.createElement("button");
+	var dAll = document.querySelector(".mark-all");
+    	dAll.appendChild(deleteAll);
+    	deleteAll.innerHTML = " Delete all ";
+
+    	markAll.addEventListener("click", markAllDone);
+    	showAll.addEventListener("click", showHide);
+		deleteAll.addEventListener("click", deleteAllButtton);
+
+    	
+    	function markAllDone() {
+			  var allItems = document.getElementsByTagName('input');
+			  for( i=0; i < allItems.length; i++) {
+			    	allItems[i].checked == 'checkbox'; 
+			      	console.log(" hey ");
+    			}
+  			}
+
+  		function showHide() {
+	        var doneItems = document.getElementsByClassName("done");
+	        console.log(doneItems);	
+
+	        for(var i=0;i< doneItems.length;i++) {
+	            doneItems[i].classList.toggle("hidden");
+	        }
+        };
+		
+
+		function deleteAllButtton() {
+		    var doneItems = document.getElementsByClassName("done");
+		    console.log(doneItems);
+			
+			for(var i=0;i< doneItems.length; ++i) {
+	        	doneItems[i].parentNode.removeChild(doneItems[i]);
+	       		 }
+	    }
+     
+}
+
+
+
+
+function updateCounter() {
+	var numberDone = document.querySelectorAll(".done").length;
+	var totalItems = document.querySelectorAll(".todo-item").length;
+	var counterComplete = document.querySelector(".counter p");
+
+	counterComplete.innerHTML = numberDone + "/" + totalItems + " Completed";
 
 }
 
 
-function done() {
-	if (this.checked) {
-		addClass(this.parentElement, "done");
-	} else{
-		removeClass(this.parentElement, "done");
-	}
+function deleteItem(){
+	var listItem = this.parentElement;
+	listItem.parentNode.removeChild(listItem);
+
+	updateCounter();
+
 }
 
 
@@ -38,7 +118,6 @@ function toggleListVisibility(toDoList) {
 		listArea.classList.add("hidden");
 	}
 }
-
 
 
 
@@ -56,5 +135,11 @@ window.onload = function () {
 		toggleListVisibility(toDoList);
 
 		formInput.value = "";
+
 	});
+
+	createThreeButtons();
 }
+
+
+
